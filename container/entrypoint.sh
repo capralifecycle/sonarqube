87a9df1c19ac51b05b1f9eb0fc6eac574f9e210f
Henrik Steen <henrist@henrist.net>
2017-07-25 02:47:04 +0200

    Remove ecr stuff not relevant

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Log in to AWS using task role that gives permission
-eval $(aws ecr get-login --no-include-email --region eu-central-1)
-
 # Get credentials from parameters store
 export SONARQUBE_JDBC_USERNAME=$(aws ssm get-parameters --region eu-central-1 --names /buildtools/sonarqube/jdbc-username | jq -r .Parameters[0].Value)
 export SONARQUBE_JDBC_PASSWORD=$(aws ssm get-parameters --region eu-central-1 --names /buildtools/sonarqube/jdbc-password --with-decryption | jq -r .Parameters[0].Value)
