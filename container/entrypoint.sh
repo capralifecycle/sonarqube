9370a4234cfcccb59c69e4122789776bbfa4c65c
Henrik Steen <henrist@henrist.net>
2019-01-15 19:20:11 +0100

    Replace deprecated env variables

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Get credentials from parameters store
-export SONARQUBE_JDBC_USERNAME=$(aws ssm get-parameters --region eu-central-1 --names /buildtools/sonarqube/jdbc-username | jq -r .Parameters[0].Value)
-export SONARQUBE_JDBC_PASSWORD=$(aws ssm get-parameters --region eu-central-1 --names /buildtools/sonarqube/jdbc-password --with-decryption | jq -r .Parameters[0].Value)
-export SONARQUBE_JDBC_URL=$(aws ssm get-parameters --region eu-central-1 --names /buildtools/sonarqube/jdbc-url | jq -r .Parameters[0].Value)
+export sonar.jdbc.username==$(aws ssm get-parameters --region eu-central-1 --names /buildtools/sonarqube/jdbc-username | jq -r .Parameters[0].Value)
+export sonar.jdbc.password==$(aws ssm get-parameters --region eu-central-1 --names /buildtools/sonarqube/jdbc-password --with-decryption | jq -r .Parameters[0].Value)
+export sonar.jdbc.url==$(aws ssm get-parameters --region eu-central-1 --names /buildtools/sonarqube/jdbc-url | jq -r .Parameters[0].Value)
 
 # Use run script included in SonarQube docker image
 exec "$SONARQUBE_HOME/bin/run.sh"
