c592f3f0f94e9274dfe360a54d20d3bdbb250907
Henrik Steen <henrist@henrist.net>
2019-01-15 20:04:26 +0100

    Workaround for dot in env name
    This also ensures the script fails correctly if the aws command fails.

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,16 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 
 ssm_get_param() {
+  set -o pipefail
   aws ssm get-parameters --region eu-central-1 --names $1 ${2:-} | jq -r .Parameters[0].Value
 }
 
 # If running tests, don't bother setting jdbc variables. It will default to embedded H2 database.
 if [ "${1:-}" != "test" ]; then
-  # Get credentials from parameters store
-  export sonar.jdbc.username=$(ssm_get_param /buildtools/sonarqube/jdbc-username)
-  export sonar.jdbc.password=$(ssm_get_param /buildtools/sonarqube/jdbc-password --with-decryption)
-  export sonar.jdbc.url=$(ssm_get_param /buildtools/sonarqube/jdbc-url)
+  # Get credentials from parameters store.
+  username=$(ssm_get_param /buildtools/sonarqube/jdbc-username)
+  password=$(ssm_get_param /buildtools/sonarqube/jdbc-password --with-decryption)
+  url=$(ssm_get_param /buildtools/sonarqube/jdbc-url)
+
+  # We cannot use "export xx.xx=xx" having a dot, so we use `env` as a
+  # workaround for setting the environment variables.
+  exec env \
+    sonar.jdbc.username="$username" \
+    sonar.jdbc.password="$password" \
+    sonar.jdbc.url="$url" \
+    "$SONARQUBE_HOME/bin/run.sh"
 fi
 
 # Use run script included in SonarQube docker image
